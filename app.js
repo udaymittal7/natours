95071299b7243a6366ff34c0e9ec963ed055c16a
udaymittal7 <mittaluday435@gmail.com>
2021-04-23 20:09:12 +0530

    implemented cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,6 +28,12 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) Global Middlewares
+
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+
+app.options('*', cors());
 
 //serving static files
 app.use(express.static(path.join(__dirname, 'public')));
