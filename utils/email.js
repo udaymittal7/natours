441c2e4ab16c135f249be1f80766df4a5108b036
udaymittal7 <mittaluday435@gmail.com>
2021-05-04 17:12:03 +0530

    fixed welcome pug error

--- utils/email.js
+++ utils/email.js
@@ -3,63 +3,66 @@ const pug = require('pug');
 const htmlToText = require('html-to-text');
 
 module.exports = class Email {
-    constructor(user, url) {
-        this.to = user.email;
-        this.firstName = user.name.split(' ')[0];
-        this.url = url;
-        this.from = `Uday Mittal <${process.env.EMAIL_FROM}>`;
-    }
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Uday Mittal <${process.env.EMAIL_FROM}>`;
+  }
 
-    newTransport() {
-        // if(process.env.NODE_ENV === 'development'){
-        return nodemailer.createTransport({
-            host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
-            auth: {
-                user: process.env.EMAIL_USERNAME,
-                pass: process.env.EMAIL_PASSWORD
-            }
-            //activate in gmail "less secure app" option
-        });
+  newTransport() {
+    // if(process.env.NODE_ENV === 'development'){
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+      //activate in gmail "less secure app" option
+    });
 
-        //SendGrid
-        // return nodemailer.createTransport({
-        //     service: 'SendGrid',
-        //     auth: {
-        //         user: process.env.SENDGRID_USERNAME,
-        //         pass: process.env.SENDGRID_PASSWORD
-        //     }
-        // });
-    }
+    //SendGrid
+    // return nodemailer.createTransport({
+    //     service: 'SendGrid',
+    //     auth: {
+    //         user: process.env.SENDGRID_USERNAME,
+    //         pass: process.env.SENDGRID_PASSWORD
+    //     }
+    // });
+  }
 
-    // send the actual email
-    async send(template, subject) {
-        // 1) Render HTML based on a pug template
-        const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
-            firstName: this.firstName,
-            url: this.url,
-            subject
-        });
+  // send the actual email
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
 
-        // 2) define the email options
-        const mailOptions = {
-            from: this.from,
-            to: this.to,
-            subject,
-            html,
-            text: htmlToText.fromString(html)
-            // html:
-        }; 
-
-        // 3) create a transport and send email
-        await this.newTransport().sendMail(mailOptions);
-    }
-
-    async sendWelcome() {
-        await this.send('Welcome', 'Welcome to the Natours family!');
+    // 2) define the email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+      // html:
     };
 
-    async sendPasswordReset() {
-        await this.send('passwordReset', 'Your password reset token (valid for only 10 minutes)')
-    };
+    // 3) create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours family!');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)'
+    );
+  }
 };
