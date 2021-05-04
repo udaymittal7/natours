4f1f4e5e71f42b1437cde5992545c5d66fe58659
udaymittal7 <mittaluday435@gmail.com>
2021-05-04 17:36:08 +0530

    fixed email.js

--- utils/email.js
+++ utils/email.js
@@ -15,6 +15,7 @@ module.exports = class Email {
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USERNAME,
         pass: process.env.EMAIL_PASSWORD,
